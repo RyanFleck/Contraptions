55351834ccf366eabfe1a188ce771410501aa023
Ryan Fleck <ryan.fleck@protonmail.com>
2020-11-04 15:19:13 -0700

    feat(main): add all letter functions back

--- contraptions/contraptions.py
+++ contraptions/contraptions.py
@@ -9,5 +9,21 @@ def subtract(a: int, b: int) -> int:
     return a - b
 
 
+def a():
+    return "x"
+
+
+def b():
+    return "x"
+
+
+def c():
+    return "x"
+
+
 def d():
-    return "d"
+    return "x"
+
+
+def e():
+    return "x"
