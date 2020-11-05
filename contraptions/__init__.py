c346ffb2b79e32cbbd3462369f01e0b841c0a251
actions-user <actions@github.com>
2020-11-05 00:36:18 +0000

    3.10.2
    Automatically generated by python-semantic-release

--- contraptions/__init__.py
+++ contraptions/__init__.py
@@ -3,7 +3,7 @@
 # Package info:
 __author__ = """Ryan Fleck"""
 __email__ = "Ryan.Fleck@protonmail.com"
-__version__ = "3.10.1"
+__version__ = "3.10.2"
 
 # Export all functions:
 from .math import *
